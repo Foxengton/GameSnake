a628d70dbe0bbfacc8637a25b9c98cfdabf5c797
Foxengton <Foxengton@yandex.ru>
2019-12-12 01:32:38 +0700

    Добавьте файлы проекта.

---
+++ GameSnake.py
@@ -0,0 +1,140 @@
+import pygame, random, os
+pygame.init()
+
+
+
+#===== КЛАССЫ И ФУНКЦИИ =====#
+# Точка
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def equal(self, other):
+        if self.x == other.x and self.y == other.y: return True
+        else: return False
+
+# Размер
+class Size:
+    def __init__(self, x = 0, y = None):
+        self.x = x
+
+        if y == None: self.y = x
+        else: self.y = y
+
+# Генерация координат яблока
+def appleGenerate():
+    point = Point(random.randrange(0, coordSize.x), random.randrange(0, coordSize.y))
+    return point
+
+def info():
+    os.system("cls") # Очистка консоли
+    print('Времени прошло: ' + str(millisecondLeft // 1000) + ' секунд') # Вывод очков в консоль
+    print('Счет: ' + str(score)) # Вывод очков в консоль
+
+
+
+#===== РАЗМЕРЫ И КООРДИНАТЫ =====#
+cellSize = 25 # Размер клетки
+screenSize = Size(900, 700) # Размер окна
+coordSize = Size(screenSize.x // cellSize, screenSize.y // cellSize) # Размер сетки
+
+position = Point(coordSize.x // 2, coordSize.y // 2) # Координаты змейки
+appleCoords = appleGenerate()
+
+
+
+#===== РАБОТА С ПОВЕРХНОСТЯМИ =====#
+# Отрисовка элементов
+def draw():
+    screenGame.fill((60, 174, 11)) # Заливка фона
+
+    pygame.draw.circle(screenGame, (255, 6, 6), (appleCoords.x * cellSize + cellSize // 2, appleCoords.y * cellSize + cellSize // 2), 12)  # Отрисовка яблока
+
+    screenGame.blit(snakeHead, (position.x * cellSize, position.y * cellSize)) # Отрисовка головы
+    # Отрисовка тела
+    for i in range (0, score):
+        screenGame.blit(snakeBody, (snakeSegments[i].x * cellSize, snakeSegments[i].y * cellSize)) # Отрисовка сегмента тела
+    
+    screenGame.blit(textScore, (700, 0)) # Отрисовка очков
+
+
+screenGame = pygame.display.set_mode((screenSize.x, screenSize.y)) # Установка экрана игры
+
+font = pygame.font.Font(None, 37) # Установка шрифта
+textScore = font.render('Счет: ', 0, (0, 0, 0)) # Строка "Счет"
+
+snakeHead = pygame.Surface((cellSize, cellSize)) # Создание головы
+snakeHead.fill((249, 180, 6)) # Заливка головы
+
+snakeBody = pygame.Surface((cellSize, cellSize)) # Создание тела
+snakeBody.fill((252, 210, 106)) # Заливка тела
+
+#===== ПЕРЕМЕННЫЕ =====#
+direction = 'None' # Клавиша, нажатая последней
+score = 0 # Число сегментов и кол-во очков
+snakeSegments = [] # Список координат сегментов змейки
+millisecondLeft = 0 # Времени прошло
+
+
+#===== МЕХАНИКА =====#
+# Управление
+def move(event,direction,score):
+    if   event.key == pygame.K_w and (score < 1 or direction != 'Down'):    direction = 'Up'
+    elif event.key == pygame.K_s and (score < 1 or direction != 'Up'):      direction = 'Down'
+    elif event.key == pygame.K_a and (score < 1 or direction != 'Right'):   direction = 'Left'
+    elif event.key == pygame.K_d and (score < 1 or direction != 'Left'):    direction = 'Right'
+    return direction
+
+# Конец игры
+def gameEnd():
+    # Столкновение со стеной
+    if (position.x < 0) or (position.x > coordSize.x) or (position.y < 0) or (position.y > coordSize.y):
+        print('Причина смерти: Столкновение со стеной') #Вывод в консоль
+        exit(0)
+
+    # Столкновение с телом
+    for segment in snakeSegments:
+        if (position.equal(segment)):
+            print('Причина смерти: Столкновение с телом') #Вывод в консоль
+            exit(0)
+
+# Цикл игры
+while 1:
+    for event in pygame.event.get(): # Получение события с модуля
+        if event.type == pygame.QUIT: exit(0) # Выход
+        elif event.type == pygame.KEYDOWN: direction = move(event, direction, score) # Если была нажата клавиша
+
+    # Смещение сегментов тела
+    if (len(snakeSegments) > 0):
+        snakeSegments.insert(0, Point(position.x, position.y))
+        snakeSegments.pop()
+
+    #===== ДВИЖЕНИЕ =====#
+    if   direction == 'Up':     position.y -= 1 # Вверх
+    elif direction == 'Down':   position.y += 1 # Вниз
+    elif direction == 'Left':   position.x -= 1 # Влево
+    elif direction == 'Right':  position.x += 1 # Вправо
+
+    
+    gameEnd() # Проверка на завершении игры
+
+    draw() # Отрисовка элементов
+
+    # Если змейка съела яблоко
+    if position.equal(appleCoords):
+        # Добавление сегмента в список
+        if len(snakeSegments) == 0: snakeSegments.append(Point(position.x, position.y))
+        else: snakeSegments.append(snakeSegments[len(snakeSegments) - 1])
+
+        appleCoords = appleGenerate(); # Создание нового яблока
+
+        score += 1 #Увеличение очков
+        textScore = font.render('Счет: ' + str(score), 0, (0, 0, 0)) # Обновление счета
+
+    # Обновление экрана
+    pygame.display.update()
+    pygame.time.delay(100)
+
+    millisecondLeft += 100
+    info()
